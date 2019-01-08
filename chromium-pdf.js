875eb6a429c3f7d548c0bf8ee8dfaf8bd0778f86
Juan Pujol <contact@juanpujol.com>
2019-01-08 03:34:37 +0300

    feat: implement pdf render

---
+++ chromium-pdf.js
@@ -0,0 +1,24 @@
+const chrome = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+
+const getPDF = async (url) => {
+  const browser = await puppeteer.launch({
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(url, {waitUntil: 'networkidle0', timeout: 3000000});
+
+  const file = await page.pdf();
+
+  await browser.close();
+
+  console.log(file);
+
+  return file;
+};
+
+module.exports = {getPDF}
---
+++ pdf.js
@@ -0,0 +1,29 @@
+const {parse} = require('url');
+const {getPDF} = require('./chromium-pdf');
+const {getInt, getUrlFromPath, isValidUrl} = require('./validator');
+
+module.exports = async function (req, res) {
+  try {
+    const {pathname = '/', query = {}} = parse(req.url, true);
+    const url = getUrlFromPath(pathname);
+
+    if (!isValidUrl(url)) {
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(`<h1>Bad Request</h1><p>The url <em>${url}</em> is not valid.</p>`);
+    } else {
+      const file = await getPDF(url);
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', 'attachment; filename=site.pdf');
+      res.end(file);
+    }
+  } catch (error) {
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>');
+
+    console.error(error.message);
+  }
+};
