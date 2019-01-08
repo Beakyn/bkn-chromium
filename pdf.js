0437fbdac6aa4e1e1db57180c14d0223348d862e
Matheus Albuquerque <matheus.brasil10@gmail.com>
2019-01-08 17:09:29 -0300

    fix(screenshot|pdf): use the full url

--- pdf.js
+++ pdf.js
@@ -1,29 +1,39 @@
-const {parse} = require('url');
-const {getPDF} = require('./chromium-pdf');
-const {getInt, getUrlFromPath, isValidUrl} = require('./validator');
+const { parse } = require("url");
+const { getPDF } = require("./chromium-pdf");
+const { getInt, getUrlFromPath, isValidUrl } = require("./validator");
 
 module.exports = async (req, res) => {
   try {
-    const {pathname = '/', query = {}} = parse(req.url, true);
+    const { pathname = "/", query = {} } = parse(req.url, true);
     const url = getUrlFromPath(pathname);
-    const date = new Date().toISOString().split('.')[0].replace(/:/g, '-');
+    const date = new Date()
+      .toISOString()
+      .split(".")[0]
+      .replace(/:/g, "-");
+
+    const finalUrl = req.url.substring(1);
 
     if (!isValidUrl(url)) {
       res.statusCode = 400;
-      res.setHeader('Content-Type', 'text/html');
-      res.end(`<h1>Bad Request</h1><p>The url <em>${url}</em> is not valid.</p>`);
+      res.setHeader("Content-Type", "text/html");
+      res.end(
+        `<h1>Bad Request</h1><p>The url <em>${url}</em> is not valid.</p>`
+      );
     } else {
-      const file = await getPDF(url);
+      const file = await getPDF(finalUrl);
 
       res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader('Content-Disposition', `attachment; filename=snapshot-${date}.pdf`);
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename=snapshot-${date}.pdf`
+      );
       res.end(file);
     }
   } catch (error) {
     res.statusCode = 500;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>');
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>Server Error</h1><p>Sorry, there was a problem</p>");
 
     console.error(error.message);
   }
